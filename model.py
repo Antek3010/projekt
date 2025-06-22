0d546edcf746715cdc30641c048d8402fb6616fa
Antek3010 <antek297@wp.pl>
2025-06-22 12:51:52 +0200

    Utworzenie kodu który definiuje pracowników

--- model.py
+++ model.py
@@ -2,3 +2,9 @@ factory = [
     {"name": "Mlekovita", "location": "Warszawa"},
     {"name": "Zakłady Azotowe", "location": "Puławy" },
 ]
+
+employees = [
+    {"name": "Anna", "location": "Warszawa", "factory": "Mlekovita"},
+    {"name": "Tomasz", "location": "Puławy", "factory": "Zakłady Azotowe"},
+]
+
